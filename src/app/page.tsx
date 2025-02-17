6cadd68f5a7a1d96e3e0a546435490e127bfed13
Ricardo Manuel Barbosa Flores <A00823815@tec.mx>
2025-02-16 23:51:02 -0600

    Tarea 1 Checkerboard RMBF  A00823815

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import Rectangle from "./components/RECTANGLE";
 
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
-      <Rectangle color="red"/>
-      <Link href="/page2" className="text-blue-500 hover:underline">
-        Go to page 2
-      </Link>
-    </div>
-  );
+export default function Page2() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-green-300 gap-6">
+        <div className="grid grid-cols-3 gap-2">
+          <Rectangle color="white" />
+          <Rectangle color="black" />
+          <Rectangle color="white" />
+          <Rectangle color="black" />
+          <Rectangle color="white" />
+          <Rectangle color="black" />
+          <Rectangle color="white" />
+          <Rectangle color="black" />
+          <Rectangle color="white" />
+        </div>
+        <Link href="/page2" className="text-blue-500 hover:underline">
+          Go to Page 2
+        </Link>
+      </div>
+    );
 }
--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -3,7 +3,7 @@ import Rectangle from "../components/RECTANGLE";
 
 export default function Page2() {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-black">
+      <div className="min-h-screen grid items-center justify-center bg-black">
         <Rectangle color="yellow" />
         <Link href="/" className="text-blue-500 hover:underline">
           Go to Home
